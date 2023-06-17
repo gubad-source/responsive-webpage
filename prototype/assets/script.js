e09730cc494c4b5719fced0580490005e9ae6b58
gubad-source <qubadmustafayev@gmail.com>
2023-06-17 19:24:44 +0400

    blog kind a

--- prototype/assets/script.js
+++ prototype/assets/script.js
@@ -4,3 +4,34 @@ for (let i = 0; i < accordion.length; i++) {
     this.classList.toggle("active");
   });
 }
+let nextBtn = document.querySelector(".slider .next");
+
+let previousBtn = document.querySelector(".slider .previous");
+
+nextBtn.addEventListener("click", function () {
+  var activeItem = document.querySelector(".slider .items .item.active");
+
+  activeItem.classList.remove("active");
+
+  if (activeItem.nextElementSibling != null) {
+    activeItem.nextElementSibling.classList.add("active");
+  } else {
+    document
+      .querySelector(".slider .items .item:first-child")
+      .classList.add("active");
+  }
+});
+
+previousBtn.addEventListener("click", function () {
+  var activeItem = document.querySelector(".slider .items .item.active");
+
+  activeItem.classList.remove("active");
+
+  if (activeItem.previousElementSibling != null) {
+    activeItem.previousElementSibling.classList.add("active");
+  } else {
+    document
+      .querySelector(".slider .items .item:last-child")
+      .classList.add("active");
+  }
+});
