eaae1f78032a789de7ac6339c0153885d4da95b5
gubad-source <qubadmustafayev@gmail.com>
2023-06-19 21:28:29 +0400

    hamburger kind a

--- prototype/assets/script.js
+++ prototype/assets/script.js
@@ -1,9 +1,17 @@
+//////////////////////////// hamburger
+let hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", () => {
+  console.log("test");
+  document.querySelector(".navbar ul").classList.toggle("disapear");
+});
+//////////////////////////// accordion
 let accordion = document.querySelectorAll(".accordion .content-box");
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
   });
 }
+//////////////////////////// slider
 let nextBtn = document.querySelector(".slider .next");
 
 let previousBtn = document.querySelector(".slider .previous");
@@ -35,6 +43,8 @@ previousBtn.addEventListener("click", function () {
       .classList.add("active");
   }
 });
+
+//////////////////////////// highlight
 let highligth = document.querySelector("p");
 
 let words = highligth.innerText.split(" ");
